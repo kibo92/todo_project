c6d98ba3fd3a597e19ece377d1f763ca33cfc45e
Bach <borislukovic@outlook.com>
2019-02-21 03:00:32 +0100

    Adding basic front template and logic

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//Check off Specific Todos by Clicking
+$("li").click(function() {
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todos
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+
+});
+
+
